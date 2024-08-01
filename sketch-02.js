a5f763433a2f44bb11d15828b17478e3d57f0e18
s19835 <s19835@sci.pdn.ac.lk>
2024-08-01 23:08:06 +0530

    create clock

--- sketch-02.js
+++ sketch-02.js
@@ -15,34 +15,26 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
-    const x = 0.5 * width;
-    const y = 0.5 * height;
+    const cx = 0.5 * width;
+    const cy = 0.5 * height;
     const w = 0.01 * width;
     const h = 0.1 * height;
-
-    // context.save();
-    // context.translate(x, y);
-    // context.rotate(0.3);
-
-    // context.beginPath();
-    // context.rect(-0.5 * w, -0.5 * h, w, h);
-    // context.fill();
-    // context.restore();
-
-    // context.translate(100, 400);
-    // context.beginPath();
-    // context.arc(width/2, height/2, 50, 0, 2 * Math.PI, false);
-    // context.fill();
+    let x, y;
     
     const num = 12;
+    const radius = width * 0.3; // radius for circle
     // declare the angle to loop
-    const slice = degToRad(360/num);
+    const slice = degToRad(360 / num);
 
     for (let i = 0; i < num; i++) {
       const angle = slice * i;
+
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
       context.save();
       context.translate(x, y);
-      context.rotate(angle);
+      context.rotate(-angle);
 
       context.beginPath();
       context.rect(-w * 0.5, -h * 0.5, w, h);
