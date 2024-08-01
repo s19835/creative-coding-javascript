07cdf2ddadc8b026a9f82e4e369e1d9e1bdbff86
s19835 <s19835@sci.pdn.ac.lk>
2024-08-01 22:33:53 +0530

    add star

--- sketch-02.js
+++ sketch-02.js
@@ -2,7 +2,11 @@ import canvasSketch from 'canvas-sketch';
 
 const settings = {
   dimensions: [ 2048, 2048 ]
-};
+}
+
+const degToRad = (degree) => {
+  return degree / 180 * Math.PI;
+}
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -13,22 +17,38 @@ const sketch = () => {
 
     const x = 0.5 * width;
     const y = 0.5 * height;
-    const w = 0.3 * width;
-    const h = 0.3 * height;
+    const w = 0.01 * width;
+    const h = 0.1 * height;
 
-    context.save();
-    context.translate(x, y);
-    context.rotate(0.3);
+    // context.save();
+    // context.translate(x, y);
+    // context.rotate(0.3);
 
-    context.beginPath();
-    context.rect(-0.5 * w, -0.5 * h, w, h);
-    context.fill();
-    context.restore();
+    // context.beginPath();
+    // context.rect(-0.5 * w, -0.5 * h, w, h);
+    // context.fill();
+    // context.restore();
 
-    context.translate(100, 400);
-    context.beginPath();
-    context.arc(width/2, height/2, 50, 0, 2 * Math.PI, false);
-    context.fill();
+    // context.translate(100, 400);
+    // context.beginPath();
+    // context.arc(width/2, height/2, 50, 0, 2 * Math.PI, false);
+    // context.fill();
+    
+    const num = 12;
+    // declare the angle to loop
+    const slice = degToRad(360/num);
+
+    for (let i = 0; i < num; i++) {
+      const angle = slice * i;
+      context.save();
+      context.translate(x, y);
+      context.rotate(angle);
+
+      context.beginPath();
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.fill();
+      context.restore();
+    }
 
   };
 };
