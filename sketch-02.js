16d442d89cf8514a26368cfe455b7ea329b284af
s19835 <s19835@sci.pdn.ac.lk>
2024-08-01 21:36:42 +0530

    add transform

--- sketch-02.js
+++ sketch-02.js
@@ -1,4 +1,4 @@
-const canvasSketch = require('canvas-sketch');
+import canvasSketch from 'canvas-sketch';
 
 const settings = {
   dimensions: [ 2048, 2048 ]
@@ -8,6 +8,23 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+
+    const x = 0.5 * width;
+    const y = 0.5 * height;
+    const w = 0.3 * width;
+    const h = 0.3 * height;
+
+    context.save();
+    context.translate(x, y);
+    context.rotate(0.3);
+
+    context.beginPath();
+    context.rect(-0.5 * w, -0.5 * h, w, h);
+    context.fill();
+    context.restore();
+
   };
 };
 
