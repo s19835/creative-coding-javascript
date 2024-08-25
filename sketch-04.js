de76226d1020727a8c3bb1ec0041985c228e8098
s19835 <s19835@sci.pdn.ac.lk>
2024-08-25 21:12:38 +0530

    add params

--- sketch-04.js
+++ sketch-04.js
@@ -1,19 +1,33 @@
-const canvasSketch = require('canvas-sketch');
-const random = require('canvas-sketch-util/random');
-const math = require('canvas-sketch-util/math');
+import canvasSketch from 'canvas-sketch';
+import { noise2D } from 'canvas-sketch-util/random';
+import { mapRange } from 'canvas-sketch-util/math';
+//import {Pane} from 'tweakpane';
 
 const settings = {
   dimensions: [ 1080, 1080 ],
   animate: true,
 };
 
+// add tweaking parameters
+const params = {
+  cols: 10,
+  rows: 10,
+  scaleMin: 1,
+  scaleMax: 30,
+  freq: 0.001,
+  amp: 0.2,
+  frame: 0,
+  animate: true,
+  lineCap: 'butt',
+}
+
 const sketch = () => {
   return ({ context, width, height, frame }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const cols = 10;
-    const rows = 10;
+    const cols = params.cols;
+    const rows = params.rows;
     const numCells = cols * rows;
 
     const gridw = width * 0.8;
@@ -33,16 +47,21 @@ const sketch = () => {
       const w = cellw * 0.8;
       const h = cellh * 0.8;
 
-      const n = random.noise2D(x, y, 0.001);
-      const angle = n * Math.PI * 0.2;
-      const scale = math.mapRange(n, -1, 1, 1, 30);
+      const f = params.animate ? frame: params.frame;
+
+      const n = noise2D(x + frame * 10, y, 0.001);
+      
+      const angle = n * Math.PI * params.amp;
+      const scale = mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
 
       context.save();
       context.translate(x + margx + cellw * 0.5, y + margy + cellh * 0.5);
       context.rotate(angle);
 
-      context.lineWidth = scale * 0.1;
+      context.lineWidth = scale;
+      context.lineCap = params.lineCap;
 
+      context.beginPath();
       context.moveTo(w * -0.5, 0);
       context.lineTo(w * 0.5, 0);
       context.stroke();
